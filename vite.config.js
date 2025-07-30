3ecc4844c7d21a86067b5c26fda91f5df72139ab
Joch-090 <eyasuzerihun80@gmail.com>
2025-07-30 10:30:11 +0300

    Update deploy scripts and vite base config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Project-002-Impact-Production/", // 👈 this is IMPORTANT
   plugins: [tailwindcss(), react()],
 });
